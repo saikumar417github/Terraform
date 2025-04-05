b1b6dfbce1177c355d6132d88549f1ba3fbe453a
sai <sai@gmail.com>
2025-04-05 14:07:35 +0530

    scripting

--- EC2/security_group.tf
+++ EC2/security_group.tf
@@ -3,10 +3,9 @@ resource "aws_security_group" "allow_ssh" {
   description = "Allow port 22 dfor SSH access"
   vpc_id      = "vpc-0b239c0bece9d117b"
 
-  #tags = {
-  #  Name = "allow_ssh"
-  # }
-  #}
+  tags = {
+    Name = "allow_ssh"
+  }
 
   egress {
     from_port        = 0
@@ -23,5 +22,14 @@ resource "aws_security_group" "allow_ssh" {
     protocol         = "tcp"
     cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
+  }
+
+  resource "aws_instance" "terrafrom" {
+    ami                    = "ami-00a929b66ed6e0de6"
+    instance_type          = "t2.micro"
+    vpc_security_group_ids = [aws_security_group.allow_ssh.id]
+    tags = {
+      Name = "terraform"
+    }
   }
 }
